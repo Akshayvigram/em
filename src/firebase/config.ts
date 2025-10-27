264f1b9b485c8e1e6561be04228541fc8f4eb61d
Raghul N.S <raghulpravee799@gmail.com>
2025-10-27 10:46:08 +0530

    Firebase Connection

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // TODO: Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDSI9Vs31cUspeV7UWAdvwcf5y3i9emzdo",
-  authDomain: "easemilker-web.firebaseapp.com",
-  projectId: "easemilker-web",
-  storageBucket: "easemilker-web.firebasestorage.app",
-  messagingSenderId: "513717812306",
-  appId: "1:513717812306:web:69c542fc0eadf4ee95b38b"
+  apiKey: "AIzaSyBdn97VzdR0lcWH4s6YOsO_F3vZx74Sge8",
+  authDomain: "easemilker-web-e2831.firebaseapp.com",
+  projectId: "easemilker-web-e2831",
+  storageBucket: "easemilker-web-e2831.firebasestorage.app",
+  messagingSenderId: "318268834461",
+  appId: "1:318268834461:web:0f8651ef7dda5140e30a74"
 };
 
 // Initialize Firebase
